b99070e18293423a59e2d0ee48f680b882047600
Zerebos <me@zerebos.com>
2024-05-27 15:49:19 -0400

    Restructure for slash commands

--- src/index.js
+++ src/index.js
@@ -1,43 +1,50 @@
-const path = require("path");
-const sqlite = require("sqlite");
-const sqlite3 = require("sqlite3");
-const Extensions = require("./ext");
-Extensions.addAll();
+const fs = require("node:fs");
+const path = require("node:path");
+
+// Require the necessary discord.js classes
+const {Client, Collection, GatewayIntentBits, ActivityType} = require("discord.js");
 
 
-const Commando = require("discord.js-commando");
-
-
-
-const client = new Commando.Client({
-    commandPrefix: process.env.BOT_PREFIX,
-    owner: process.env.BOT_OWNER_ID,
-    fetchAllMembers: process.env.BOT_FETCH_MEMBERS || false,
-    description: process.env.BOT_DESCRIPTION,
-    invite: process.env.BOT_INVITE
+// Create a new client instance
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent,
+        GatewayIntentBits.GuildMembers,
+        GatewayIntentBits.GuildVoiceStates
+    ],
+    presence: {activities: [{name: "for spam", type: ActivityType.Watching}]}
 });
 
 
+client.commands = new Collection();
+// For nested folders
+// const commandFolders = fs.readdirSync(path.join(__dirname, "commands")).filter(d => fs.statSync(path.join(__dirname, "commands", d)).isDirectory());
+// const commandFiles = commandFolders.map(f => fs.readdirSync(path.join(__dirname, "commands", f)).filter(file => file.endsWith(".js")).map(c => path.join(__dirname, "commands", f, c)));
 
-client.registry.registerGroups([
-    ["botadmin", "Bot Admin"],
-    ["meta", "Meta"],
-    ["moderation", "Moderation"],
-    ["assignableroles", "Assignable Roles"],
-]);
+const commandsPath = path.join(__dirname, "commands");
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith(".js"));
 
-client.registry.registerDefaultTypes();
-client.registry.registerDefaultGroups();
-client.registry.registerDefaultCommands({
-    prefix: false,
-    help: false, 
-    ping: false,
-    eval: false,
-    unknownCommand: false
-});
+for (const file of commandFiles) {
+    // For nested folders
+    // const command = require(file);
+    const filePath = path.join(commandsPath, file);
+    const command = require(filePath);
+    // Set a new item in the Collection
+    // With the key as the command name and the value as the exported module
+    client.commands.set(command.data.name, command);
+}
 
-client.registry.registerCommandsIn(path.join(__dirname, "commands"));
 
-client.setProvider(sqlite.open({driver: sqlite3.Database, filename: path.join(__dirname, "..", "settings.sqlite3")}).then(db => new Commando.SQLiteProvider(db))).catch(console.error);
+const eventsPath = path.join(__dirname, "events");
+const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith(".js"));
+
+for (const file of eventFiles) {
+    const filePath = path.join(eventsPath, file);
+    const event = require(filePath);
+    if (event.once) client.once(event.name, (...args) => event.execute(...args));
+    else client.on(event.name, (...args) => event.execute(...args));
+}
 
 client.login(process.env.BOT_TOKEN);
